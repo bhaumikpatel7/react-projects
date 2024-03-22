2b5bca7b32de4f41e7a59587ffc2e3b4be2c39ae
bhaumikpatel7 <72443943+bhaumikpatel7@users.noreply.github.com>
2024-03-22 18:58:20 -0400

    plan with love

--- plan-with-love/src/App.js
+++ plan-with-love/src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import data from "./data";
+import Tours from "./components/Tours";
+import { useState } from "react";
+import Card from "./components/Card";
 
 const App = () => {
-  return <div>App</div>;
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0){
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button  className="btn-white" onClick={()=> setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Tours tours={tours} removeTour = {removeTour}></Tours>
+    </div>
+  );
 };
 
 export default App;
