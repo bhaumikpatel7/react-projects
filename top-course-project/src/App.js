1ea7a5bd41d04f7af4aca3625b7950919df2fff2
bhaumikpatel7 <72443943+bhaumikpatel7@users.noreply.github.com>
2024-03-23 17:20:03 -0400

    top course part 1

--- top-course-project/src/App.js
+++ top-course-project/src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Cards from "./components/Cards";
+import Filter from "./components/Filter";
+import { apiUrl, filterData } from "./data";
+import Spinner from './components/Spinner'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
-  return <div>App</div>;
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+
+      setCourses(output);
+    } catch (error) {
+      toast.error("Network error");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Filter filterData={filterData} />
+      </div>
+      <div>
+        {
+          loading ? (<Spinner/>) : (<Cards courses={courses} />)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ top-course-project/src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Spinner.css";
+
+function Spinner() {
+  return (
+    <div>
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Spinner;
